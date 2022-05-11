266d2fe8304b274b4b2efdc786ded633c51c74ff
Qi Da Chen (Mike) <83503846+AnbuCap@users.noreply.github.com>
2022-05-10 22:27:20 -0400

    added

--- project/interpreter.java
+++ project/interpreter.java
@@ -6,21 +6,28 @@ import java.util.Scanner;
 
 public class interpreter {
     public static void main(String[] args) throws FileNotFoundException {
+        //int counter = 0;
         String message = " ";
         //String[] store;
         File file = new File("E://GitHub//CISC3160//project//inputOne.txt");
         Scanner scan = new Scanner(file);
         while(scan.hasNextLine()) {
             message = message.concat(scan.nextLine());
+            //message.charAt()
         }
         for(int i = 0; i < message.length(); i++) {
-            
+            char character = message.charAt(i);
+            if(character == ';') {
+                /*store[counter] = message;
+                counter++;*/
+                
+            }
         }
         System.out.println(message);
         scan.close();
     }
 
-    public static void isDight() {
+    public static void Dight() {
 
     }
 
