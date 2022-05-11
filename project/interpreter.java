f7bff8fb0012207bfa4749b80783cb7ee158ce4d
Qi Da Chen (Mike) <83503846+AnbuCap@users.noreply.github.com>
2022-05-10 22:49:42 -0400

    blah

--- project/interpreter.java
+++ project/interpreter.java
@@ -7,19 +7,28 @@ import java.util.Scanner;
 public class interpreter {
     public static void main(String[] args) throws FileNotFoundException {
         //int counter = 0;
-        String message = " ";
+        String message = "";
+        //line;
         //String[] store;
         File file = new File("E://GitHub//CISC3160//project//inputOne.txt");
         Scanner scan = new Scanner(file);
+        StringBuilder newMessage = new StringBuilder();
         while(scan.hasNextLine()) {
             message = message.concat(scan.nextLine());
             //message.charAt()
         }
         for(int i = 0; i < message.length(); i++) {
             char character = message.charAt(i);
+            newMessage.append(character);
             if(character == ';') {
-                /*store[counter] = message;
-                counter++;*/
+                System.out.println("testing"); // this is working DONT TOUCH
+                //line = newMessage;
+                System.out.println("line is: " + newMessage);
+                /*for(int j = 0; j < line.length(); j++) {
+                    char charScan = line.charAt(i);
+                    check(charScan);
+                }*/
+                newMessage.setLength(0); // this is working. DONT TOUCH
                 
             }
         }
@@ -27,7 +36,18 @@ public class interpreter {
         scan.close();
     }
 
-    public static void Dight() {
+    public static void check(char charScan) {
+        /*for(int i = 0; i < charScan.length(); i++) {
+            char character = charScan.charAt(i);
+        }*/
+        if(Character.isDigit(charScan)) {
+            System.out.println("testing");
+        }
+    }
+
+
+
+    /*public static void Dight() {
 
     }
 
@@ -37,6 +57,6 @@ public class interpreter {
 
     public static void literal() {
 
-    }
+    }*/
 
 }
