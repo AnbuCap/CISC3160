2d9440f202f1a6109e708e5b5c58e0bc5ceafd38
Qi Da Chen (Mike) <83503846+AnbuCap@users.noreply.github.com>
2022-05-14 00:59:34 -0400

    Track =

--- project/interpreter.java
+++ project/interpreter.java
@@ -12,11 +12,13 @@ public class interpreter {
     public static void main(String[] args) throws FileNotFoundException {
         //int counter = 0;
         String message = "";
+        String messageTwo;
         //line;
         //String[] store;
         File file = new File("E://GitHub//CISC3160//project//inputOne.txt");
         Scanner scan = new Scanner(file);
         StringBuilder newMessage = new StringBuilder();
+        StringBuilder newMessageTwo = new StringBuilder();
         while(scan.hasNextLine()) {
             message = message.concat(scan.nextLine());
             //message.charAt()
@@ -26,17 +28,22 @@ public class interpreter {
         
         for(int i = 0; i < message.length(); i++) {
             char character = message.charAt(i);
-            newMessage.append(character);
-            if(character == ';') {
-                // System.out.println("testing"); // this is working DONT TOUCH
-                //line = newMessage;
-                // System.out.println("line is: " + newMessage);
-                for(int j = 0; j < newMessage.length(); j++) {
-                    char charScan = newMessage.charAt(j);
-                    check(charScan);
+            if(character == '=') {
+                for(int k = i + 2; k < message.length(); k++) {         
+                    newMessage.append(message.charAt(k));    
                 }
-                newMessage.setLength(0); // this is working. DONT TOUCH
-                
+                messageTwo = newMessage.toString();
+                for(int j = 0; j < messageTwo.length(); j++) {
+                    char characterTwo = messageTwo.charAt(j);
+                    newMessageTwo.append(characterTwo);
+                    if(characterTwo == ';') {
+                        for(int c = 0; c < newMessageTwo.length(); c++) {
+                            char charScan = newMessageTwo.charAt(c);
+                            check(charScan);
+                        }
+                        newMessage.setLength(0); // this is working. DONT TOUCH
+                    }
+                }
             }
         }
         System.out.println(message);
